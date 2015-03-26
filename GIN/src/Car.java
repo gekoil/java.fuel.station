777796398c164ee510117ca29a0ccc1ceb592645
Yevgeni Strongin <yevgeni.strongin@sap.com>
2015-03-26 17:54:13 +0200

    Added id field and generation to Car

--- GIN/src/Car.java
+++ GIN/src/Car.java
@@ -1,6 +1,8 @@
 
 public class Car {
-	
+
+    private static int carCount;
+    private int id;
 	private int fuel;
 	private boolean needWash = false;
 	
@@ -8,6 +10,7 @@ public class Car {
 		if(Math.random()%2 == 0)
 			needWash = true;
 		fuel = (int) Math.random()*41;
+        id = carCount++;
 	}
 
 	public int getFuel() {
@@ -25,5 +28,8 @@ public class Car {
 	public void setWash(boolean wash) {
 		this.needWash = wash;
 	}
-	
+
+    public int getId() {
+        return id;
+    }
 }
