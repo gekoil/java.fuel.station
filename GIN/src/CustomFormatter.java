91a4e0508fbb0b83063306f1ab5a109a4648e692
Itamar <itmrm7@gmail.com>
2015-04-09 11:08:13 +0300

    New Time formatter.

--- GIN/src/CustomFormatter.java
+++ GIN/src/CustomFormatter.java
@@ -1,5 +1,8 @@
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
@@ -7,12 +10,12 @@ public class CustomFormatter extends Formatter {
     @Override
     public String format(LogRecord record) {
         // dd/MM/yyyy HH:mm:ss
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-        StringBuffer buffer = new StringBuffer();
-        //LocalDate date = LocalDate.now();
-        buffer.append(formatter.format(LocalDate.now()));
+    	LocalDateTime time = LocalDateTime.now();
+    	DateTimeFormatter formater = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        StringBuffer buffer = new StringBuffer(100);
+        buffer.append(time.format(formater));
         buffer.append(" " + record.getLevel() + " ");
-        buffer.append(formatMessage(record));
+        buffer.append(formatMessage(record) + '\n');
         return buffer.toString();
     }
 }
