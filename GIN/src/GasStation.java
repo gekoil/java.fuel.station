4ba924e02b4f0bbb785eac57ae0312239a908e99
Itamar <itmrm7@gmail.com>
2015-04-09 10:35:30 +0300

    I added Handler.

--- GIN/src/GasStation.java
+++ GIN/src/GasStation.java
@@ -1,10 +1,13 @@
+import java.io.IOException;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class GasStation extends Thread{
     private static final Logger log = Logger.getLogger(GasStation.class.getName());
+    private final String FILE_HANDLER = GasStation.class.getName() + ".xml";
 
     private CarWash carWash;
     private ArrayList<Pump> fuelPumps;
@@ -16,6 +19,7 @@ public class GasStation extends Thread{
     private boolean isWorking;
 
     public GasStation(CarWash carWash, ArrayList<Pump> fuelPumps, int fuelReserve, int fuelCapacity) {
+    	initLog();
         this.carWash = carWash;
         carWash.setStation(this);
         this.fuelPumps = fuelPumps;
@@ -27,18 +31,31 @@ public class GasStation extends Thread{
 
     @Override
     public void run() {
-    	log.log(Level.INFO, "The Station is open");
+    	log.log(Level.INFO, "The Station is open.");
     	carWash.start();
         while(isWorking || !incomingCars.isEmpty()) {
         	if(!incomingCars.isEmpty())
         		organizer(incomingCars.pollFirst());
         }
+        log.log(Level.INFO, "Start a closing procedure.");
         try {
 			carWash.join();
 			for(Pump p : fuelPumps) {
 				p.join();
 			}
 		} catch (InterruptedException e) {
+			log.log(Level.WARNING, e.getMessage());
+		}
+    }
+    
+    private void initLog() {
+    	FileHandler handler;
+    	try {
+    		handler = new FileHandler(FILE_HANDLER);
+    		handler.setFormatter(new CustomFormatter());
+    		log.addHandler(handler);
+    		log.setUseParentHandlers(false);
+    	} catch (SecurityException | IOException e) {
 			e.getMessage();
 		}
     }
